71f63dbb3a3435fff3798742fda444cc3588535a
Kostas4S <81780796+Kostas4S@users.noreply.github.com>
2021-12-28 18:05:05 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAePEoogY80nXZuRFQ_1HQW8tDboeefKok",
+    authDomain: "animalfact-3534c.firebaseapp.com",
+    projectId: "animalfact-3534c",
+    storageBucket: "animalfact-3534c.appspot.com",
+    messagingSenderId: "233634186351",
+    appId: "1:233634186351:web:043fc4e4f1df8ff89a799a"
+  };
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
